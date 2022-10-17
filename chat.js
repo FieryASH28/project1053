d253b54204423e5a89fa0dd6cd133eeb6a08c0af
FieryASH28 <102391985+FieryASH28@users.noreply.github.com>
2022-10-17 17:07:50 -0500

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,16 @@
+// Your web app's Firebase configuration
+//ADD YOUR FIREBASE LINKS HERE
+
+
+
+function addUser()
+{
+    
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "chat_room.html";
+    
+}
+
+
+
